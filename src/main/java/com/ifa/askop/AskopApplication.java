73a8627c9ab392770988bea2c30d0938c8b2cb84
atagunay <atafb2000gunay@gmail.com>
2022-02-27 13:28:38 +0300

    Initial commit

---
+++ src/main/java/com/ifa/askop/AskopApplication.java
@@ -0,0 +1,13 @@
+package com.ifa.askop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifa/askop/AskopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifa.askop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
