aca97ebe6088191e2c4220f14b4494156b01a7e5
ismailkrc57 <ismailkrc57@gmail.com>
2022-03-01 12:01:55 +0300

    Resulting yapısı oluşturuldu

---
+++ src/main/java/com/ifa/askop/core/utilities/results/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.ifa.askop.core.utilities.results;
+
+public class ErrorResult extends Result{
+    public ErrorResult( String message) {
+        super(false, message);
+    }
+
+    public ErrorResult() {
+        super(false);
+    }
+}
