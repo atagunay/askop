3923e419d350812bc60f7fbb58e61f97cebf528b
Ata Günay <180704020@st.maltepe.edu.tr>
2022-03-11 13:28:43 +0300

    inventory için repo-servis-controller olusturuldu

---
+++ src/main/java/com/ifa/askop/business/Abstract/IInventoryService.java
@@ -0,0 +1,7 @@
+package com.ifa.askop.business.Abstract;
+
+import com.ifa.askop.core.business.IServiceBase;
+import com.ifa.askop.entity.model.Inventory;
+
+public interface IInventoryService extends IServiceBase<Inventory> {
+}
--- src/main/java/com/ifa/askop/controller/InventoryController.java
+++ src/main/java/com/ifa/askop/controller/InventoryController.java
@@ -1,9 +1,16 @@
 package com.ifa.askop.controller;
 
+import com.ifa.askop.business.concrete.InventoryManager;
+import com.ifa.askop.core.controller.BaseController;
+import com.ifa.askop.entity.model.Inventory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/inventory")
-public class InventoryController {
+public class InventoryController extends BaseController<Inventory, InventoryManager> {
+
+    public InventoryController(InventoryManager serviceBase) {
+        super(serviceBase);
+    }
 }
